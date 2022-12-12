0ad23c3a031bccdd93632a25ee908e586410d5d9
aditya_kk <adityakrishnakumar12@gmail.com>
2022-12-12 18:42:22 +0530

    Added Expenses main component

---
+++ src/components/Expenses.js
@@ -0,0 +1,17 @@
+import ExpenseItem from "./ExpenseItem";
+
+import "./Expenses.css";
+
+function Expenses(props) {
+  return (
+    <div className="expenses">
+      <ExpenseItem
+        title={props.item[0].title}
+        amount={props.item[0].amount}
+        date={props.item[0].date}
+      ></ExpenseItem>
+    </div>
+  );
+}
+
+export default Expenses;
