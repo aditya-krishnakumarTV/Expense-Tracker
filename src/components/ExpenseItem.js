63b85b1f3ab892849ea0156ce56b9c8e14855314
aditya_kk <adityakrishnakumar12@gmail.com>
2022-12-12 17:51:52 +0530

    Outputting values dynamically

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem() {
+  const expenseDate = new Date();
+  const expenseTitle = "Charger";
+  const expenseAmount = 200;
+
   return (
     <div className="expense-item">
-      <div>Date</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Title</h2>
-        <div className="expense-item__price">Amount</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">₹{expenseAmount}</div>
       </div>
     </div>
   );
