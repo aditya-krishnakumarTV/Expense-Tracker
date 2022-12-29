53ae9a8f17fb2e9e5de823097f8b7985d70c7564
aditya_kk <adityakrishnakumar12@gmail.com>
2022-12-29 19:14:15 +0530

    Added ExpenseFilter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,32 +1,47 @@
+import React, { useState } from "react";
+
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpenseFilter";
 
 import "./Expenses.css";
 
 function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState("2022");
+
+  const expenseFilterHandler = (expenseFilterYear) => {
+    setFilteredYear(expenseFilterYear);
+  };
+
   return (
-    <Card className="expenses">
-      <ExpenseItem
-        title={props.item[0].title}
-        amount={props.item[0].amount}
-        date={props.item[0].date}
-      />
-      <ExpenseItem
-        title={props.item[1].title}
-        amount={props.item[1].amount}
-        date={props.item[1].date}
-      />
-      <ExpenseItem
-        title={props.item[2].title}
-        amount={props.item[2].amount}
-        date={props.item[2].date}
-      />
-      <ExpenseItem
-        title={props.item[3].title}
-        amount={props.item[3].amount}
-        date={props.item[3].date}
-      />
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          defaultSelected={filteredYear}
+          onExpenseFilterSelect={expenseFilterHandler}
+        />
+        <ExpenseItem
+          title={props.item[0].title}
+          amount={props.item[0].amount}
+          date={props.item[0].date}
+        />
+        <ExpenseItem
+          title={props.item[1].title}
+          amount={props.item[1].amount}
+          date={props.item[1].date}
+        />
+        <ExpenseItem
+          title={props.item[2].title}
+          amount={props.item[2].amount}
+          date={props.item[2].date}
+        />
+        <ExpenseItem
+          title={props.item[3].title}
+          amount={props.item[3].amount}
+          date={props.item[3].date}
+        />
+      </Card>
+    </div>
   );
 }
 
